81e759ea7b468e85ea280164315b4803790ebc92
Suresh Ranjula <50176787+wssranjula@users.noreply.github.com>
2023-09-03 14:00:55 +0530

    Update csv_agent.py

--- csv_agent.py
+++ csv_agent.py
@@ -4,36 +4,44 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 from langchain.agents import create_csv_agent
-from langchain.document_loaders.csv_loader import CSVLoader
-import pandas as pd
 
-load_dotenv()
+
+
+
+
+st.set_page_config(page_title="Ask CSV")
+st.header("Ask your Question 💬")
+
+uploaded_file = st.file_uploader("Upload a CSV File",type=["csv"])
+
+if uploaded_file:
+    st.write("File uploaded successfully")
+    
 
 def main():
-    st.set_page_config(page_title="Ask CSV")
-    st.header("Ask your Question 💬")
-    uploaded_file = st.file_uploader("Upload a CSV File", type=["csv"])
-    api = st.secrets.get("OPENAI_API_KEY")
+    load_dotenv()
+    
+    if uploaded_file is not None:
+       llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+       
+       path = uploaded_file.name
+      
 
-    if uploaded_file:
-        st.write("File uploaded successfully")
-        df = pd.read_csv(uploaded_file)
-        
-        llm = ChatOpenAI(temperature=0, openai_api_key=api, model="gpt-3.5-turbo-0613")
-        data = df.to_dict()
-        agent = create_csv_agent(
-            llm,
-            data,
+       agent = create_csv_agent(
+        ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+            path,
             verbose=True,
             agent_type=AgentType.OPENAI_FUNCTIONS,
-        )
-
-        prompt = st.text_input("Your question:")
-        if prompt:
-            st.write("🧠 Thinking...")
-            st_callback = StreamlitCallbackHandler(st.container())
-            response = agent.run(prompt, callbacks=[st_callback])
-            st.write(response)
+)                        
+        
+       if prompt := st.chat_input():
+            st.chat_message('user').write(prompt)
+            with st.chat_message("assistant"):
+                st.write("🧠 Thinking...")
+                st_callback =StreamlitCallbackHandler(st.container())
+                response = agent.run(prompt,callbacks=[st_callback])
+                st.write(response)
             
+        
 if __name__ == '__main__':
     main()
